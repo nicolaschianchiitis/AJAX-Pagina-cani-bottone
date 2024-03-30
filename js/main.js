30b0e6a72c90f9cda4f1ccb200d183fc4f528b6e
Nicola Schianchi <>
2024-03-30 12:32:58 +0100

    Creata pagina

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Autore: Nicola Schianchi
+function creaCard(){
+    let container = document.getElementById("containerImg");
+    while (container.hasChildNodes()){
+        container.removeChild(container.lastElementChild);
+    }
+    let msgErrore = document.getElementById("msgErrore");
+    msgErrore.style.display = "none";
+    let card = document.createElement("div");
+    card.className = "card";
+    card.style = "margin-top: -3.5%;"
+    let img = document.createElement("img");
+    img.className = "card-img-top rounded";
+    let url = "https://dog.ceo/api/breeds/image/random";
+    fetch(url)
+    .then(
+        promise => {
+            if (promise.ok){
+                return promise.json();
+            }
+        }
+    )
+    .catch(
+        errore => msgErrore.style.display = "block"
+    )
+    .then(
+        json => {
+            img.src = json.message;
+        }
+    );
+    img.alt = "Cane";
+    card.appendChild(img);
+    container.appendChild(card);
+}
